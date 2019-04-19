b71983b85e8aaa77ad42a1e105458af0f52639c9
Inem Patrick <emmanuelpatrick25@gmail.com>
2019-04-19 18:38:19 +0100

    Add starwars logo on page first load

--- src/Movies/MovieSelectOptions.jsx
+++ src/Movies/MovieSelectOptions.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import logo from "../logo.png";
 
 const MovieSelectOptions = ({ movies, handleMoviesChange }) => {
   return (
-    <select defaultValue="" onChange={handleMoviesChange}>
-      <option value="" disabled>
-        Select a movie
-      </option>
-      {movies.map((movie, index) => (
-        <option key={movie.title} value={index}>
-          {movie.title}
+    <div className="flex-column">
+      <select defaultValue="" onChange={handleMoviesChange}>
+        <option value="" disabled>
+          Select a movie
         </option>
-      ))}
-    </select>
+        {movies.map((movie, index) => (
+          <option key={movie.title} value={index}>
+            {movie.title}
+          </option>
+        ))}
+      </select>
+      <img src={logo} alt="logo" width="200" height="150" />
+    </div>
   );
 };
 
